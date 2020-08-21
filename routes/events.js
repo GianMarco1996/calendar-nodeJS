96bedc4df4c8f136424d503e3e5c46d82c47d1cd
GianMarco1996 <jianmarco1996@gmail.com>
2020-08-20 19:46:15 -0500

    Backend 1.0 terminado

---
+++ routes/events.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { getEventos, crearEvento,
+    actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
+
+const router = Router();
+
+router.use(validarJWT);
+
+router.get('/', getEventos);
+
+router.post('/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de Inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de Finalización es obligatoria').custom(isDate),
+        validarCampos
+    ], crearEvento);
+
+router.put('/:id', actualizarEvento);
+
+router.delete('/:id', eliminarEvento);
+
+
+module.exports = router;
